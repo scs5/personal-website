71ad5e043ad9a188b6eb1508c28f694976a0416b
Sam Smith <scs5@clemson.edu>
2023-04-28 15:32:41 -0400

    Add skeleton structure
    Add simple outline for website structure. One page with three sections: home, projects, and about.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import About from "./pages/About"
+import { HashLink as Link } from "react-router-hash-link"
 
 function App() {
   return (
-    <h1>Sam Smith's Website</h1>
+    <BrowserRouter>
+      <div className='app-container'>
+        <Link to='#home'> Home Link</Link>
+        <Link to='#about'> About Link</Link>
+        < Home />
+        < Projects />
+        < About />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Projects.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Projects.css"
+
+function Projects() {
+  return (
+    <section id="projects">
+      <div>
+        <h1 className="text">Projects</h1>
+        <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio morbi quis commodo odio aenean sed adipiscing diam. Non diam phasellus vestibulum lorem. Et tortor consequat id porta nibh. Odio tempor orci dapibus ultrices in iaculis nunc sed. Quis lectus nulla at volutpat. Vel quam elementum pulvinar etiam non. Mi proin sed libero enim sed faucibus turpis in eu. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Malesuada fames ac turpis egestas integer. Facilisis gravida neque convallis a cras semper. Nisi quis eleifend quam adipiscing. Sit amet justo donec enim diam vulputate. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Sed vulputate mi sit amet mauris. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. </p>
+      </div>
+    </section>
+  )
+}
+
+export default Projects;
