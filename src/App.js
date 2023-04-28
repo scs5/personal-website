343d12099deaa1ed451affa181cb3a5190bdbf5b
Sam Smith <scs5@clemson.edu>
2023-04-28 17:49:13 -0400

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
 import About from "./pages/About"
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='app-container'>
-        < Home />
-        < Projects />
-        < About />
-      </div>
-    </BrowserRouter>
+    <div className='app-container'>
+      < NavBar />
+      < Home />
+      < Projects />
+      < About />
+    </div>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { HashLink as Link } from 'react-router-hash-link';
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className='nav'>
+        <Link to="#home" className='site-logo'>
+          <img src={require('../assets/mushroom.png')} alt='logo' className='logo'/>
+        </Link>
+        <ul>
+          <li>
+            <Link to="#about">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar;
