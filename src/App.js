c8689cd15fe209d51777642923d013f62aff82c6
Sam Smith <scs5@clemson.edu>
2023-04-28 15:47:08 -0400

    Home page styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import './App.css';
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
 import About from "./pages/About"
-import { HashLink as Link } from "react-router-hash-link"
 
 function App() {
   return (
     <BrowserRouter>
       <div className='app-container'>
-        <Link to='#home'> Home Link</Link>
-        <Link to='#about'> About Link</Link>
         < Home />
         < Projects />
         < About />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,10 @@ import "./Home.css"
 
 function Home() {
   return (
-    <section id="home">
+    <section className="home-container" id="home">
       <div>
-        <h1 className="text">Home</h1>
-        <p className="text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio morbi quis commodo odio aenean sed adipiscing diam. Non diam phasellus vestibulum lorem. Et tortor consequat id porta nibh. Odio tempor orci dapibus ultrices in iaculis nunc sed. Quis lectus nulla at volutpat. Vel quam elementum pulvinar etiam non. Mi proin sed libero enim sed faucibus turpis in eu. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Malesuada fames ac turpis egestas integer. Facilisis gravida neque convallis a cras semper. Nisi quis eleifend quam adipiscing. Sit amet justo donec enim diam vulputate. Dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Sed vulputate mi sit amet mauris. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. </p>
+        <h1 className="name">Sam Smith</h1>
+        <p className="intro"> Welcome to my portfolio! </p>
       </div>
     </section>
   )
