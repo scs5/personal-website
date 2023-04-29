7de561f50a1a659304a8904fcdd31d3c58e92e28
Sam Smith <scs5@clemson.edu>
2023-04-29 16:42:18 -0400

    Add About section content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,32 @@
 import React from "react"
 import "./About.css"
+import AboutImg from "../assets/mushroom.png"
 
 function About() {
   return (
     <section id="about">
-      <div>
-        <h1>About</h1>
+      <div className="about-container">
+        <div className="left-section">
+          <div className="about-info">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+            ut aliquip ex ea commodo consequat.
+            <br /><br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+            ut aliquip ex ea commodo consequat.
+            <br /><br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+            ut aliquip ex ea commodo consequat.
+          </div>
+        </div>
+        <div className="right-section">
+          <img src={AboutImg} alt="profile picture" />
+        </div>
       </div>
     </section>
   )
