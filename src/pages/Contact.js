2e949aa2a0929a7f17b11e6d97c837574219530a
Sam Smith <scs5@clemson.edu>
2023-04-29 21:01:10 -0400

    Style about and contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import "./Contact.css"
+import React, { useState } from "react";
+import "./Contact.css";
 
 function Contact() {
   const [name, setName ] = useState('');
@@ -11,50 +11,56 @@ function Contact() {
       <h1 className="section-title"> Contact </h1>
 
       <div className="contact-container">
-        <form autoComplete="off">
-          <div className="form-group">
-            <label htmlFor="name"> Your name
-              <input 
-                autoComplete="off"
-                type="text" 
-                id="name" 
-                name="name" 
-                value={name}
-                onChange={e => setName(e.target.value)}
-                autocomplete="nope"
-              />
-            </label>
+        <form autoComplete="off" className="contact-form">
+          <div className="left-section">
+            <div className="form-group">
+              <label htmlFor="name"> Your name
+                <input 
+                  type="text" 
+                  id="name" 
+                  name="name" 
+                  value={name}
+                  onChange={e => setName(e.target.value)}
+                  autoComplete="nope"
+                />
+              </label>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email"> Your email
+                <input 
+                  type="text" 
+                  id="email" 
+                  name="email" 
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  autoComplete="nope"
+                />
+              </label>
+            </div>
           </div>
 
-          <div className="form-group">
-            <label htmlFor="email"> Your email
-              <input 
-                type="text" 
-                id="email" 
-                name="email" 
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                autocomplete="nope"
-              />
-            </label>
+          <div className="right-section">
+            <div className="form-group">
+              <label htmlFor="message"> Your message
+                <textarea 
+                  type="text" 
+                  id="message" 
+                  name="message" 
+                  value={message}
+                  onChange={e => setMessage(e.target.value)}
+                />
+              </label>
+            </div>
           </div>
-
-          <div className="form-group">
-            <label htmlFor="message"> Your message
-              <textarea 
-                type="text" 
-                id="message" 
-                name="message" 
-                value={message}
-                onChange={e => setMessage(e.target.value)}
-              />
-            </label>
-          </div>
-
-          <button type="submit"> Send </button>
         </form>
+
+        <div className="button-section">
+          <button type="submit"> Send </button>
+        </div>
       </div>
     </section>
+
   )
 }
 
