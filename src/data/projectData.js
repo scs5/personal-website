a1a96465ac3a36b04ede7171954e3e7b68163ef3
scsmit23 <scs5@clemson.edu>
2023-05-03 16:55:05 -0400

    Update project images

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,7 +2,7 @@ export const projectData = [
   {
     id: 1,
     name: "Project 1",
-    imageSrc: require("../assets/maplestory.jpg"),
+    imageSrc: require("../assets/fractal1.jpg"),
     githubLink: "https://github.com",
     demoLink: "https://demo.com",
     toolsUsed: ["React", "Node.js", "MongoDB"],
@@ -12,7 +12,7 @@ export const projectData = [
   {
     id: 2,
     name: "Project 2",
-    imageSrc: require("../assets/maplestory.jpg"),
+    imageSrc: require("../assets/fractal2.jpg"),
     githubLink: "https://github.com",
     demoLink: "https://demo.com",
     toolsUsed: ["React", "Node.js", "MongoDB"],
@@ -22,7 +22,7 @@ export const projectData = [
   {
     id: 3,
     name: "Project 3",
-    imageSrc: require("../assets/maplestory.jpg"),
+    imageSrc: require("../assets/fractal3.png"),
     githubLink: "https://github.com",
     demoLink: "https://demo.com",
     toolsUsed: ["React", "Node.js", "MongoDB"],
@@ -32,7 +32,7 @@ export const projectData = [
   {
     id: 4,
     name: "Project 4",
-    imageSrc: require("../assets/maplestory.jpg"),
+    imageSrc: require("../assets/fractal4.png"),
     githubLink: "https://github.com",
     demoLink: "https://demo.com",
     toolsUsed: ["React", "Node.js", "MongoDB"],
@@ -42,7 +42,7 @@ export const projectData = [
   {
     id: 5,
     name: "Project 5",
-    imageSrc: require("../assets/maplestory.jpg"),
+    imageSrc: require("../assets/fractal5.jpg"),
     githubLink: "https://github.com",
     demoLink: "https://demo.com",
     toolsUsed: ["React", "Node.js", "MongoDB"],
@@ -52,11 +52,41 @@ export const projectData = [
   {
     id: 6,
     name: "Project 6",
-    imageSrc: require("../assets/maplestory.jpg"),
+    imageSrc: require("../assets/fractal6.jpg"),
     githubLink: "https://github.com",
     demoLink: "https://demo.com",
     toolsUsed: ["React", "Node.js", "MongoDB"],
     categories: ["Data Viz", "Deep Learning"],
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
   },
+  {
+    id: 7,
+    name: "Project 7",
+    imageSrc: require("../assets/fractal7.jpg"),
+    githubLink: "https://github.com",
+    demoLink: "https://demo.com",
+    toolsUsed: ["React", "Node.js", "MongoDB"],
+    categories: ["Data Viz"],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: 8,
+    name: "Project 8",
+    imageSrc: require("../assets/fractal8.jpg"),
+    githubLink: "https://github.com",
+    demoLink: "https://demo.com",
+    toolsUsed: ["React", "Node.js", "MongoDB"],
+    categories: ["Deep Learning"],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: 9,
+    name: "Project 9",
+    imageSrc: require("../assets/fractal9.jpg"),
+    githubLink: "https://github.com",
+    demoLink: "https://demo.com",
+    toolsUsed: ["React", "Node.js", "MongoDB"],
+    categories: ["Computer Vision"],
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  }
 ];
