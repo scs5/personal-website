3682b13ad1eb9da07280c11688de128b9135a142
Sam Smith <scs5@clemson.edu>
2023-05-02 22:47:33 -0400

    Add disappearing navigation bar
    Navigation bar disappears on scroll down.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HashLink } from 'react-router-hash-link';
 import { animateScroll as scroll } from 'react-scroll';
 import "./NavBar.css"
 
 const NavBar = () => {
   const scrollDuration = 500;
+  const hideNavHeight = 300;
+
+  const [ showNav, setShowNav ] = useState(true);
+  const [ scrollData, setScrollData ] = useState( {
+    y: 0,
+    lastY: 0
+  })
+
+  useEffect(() =>{
+    const handleScroll = () => {
+      setScrollData(prevState => {
+        return {
+          y: window.scrollY,
+          lastY: prevState.y
+        }
+      })
+    }
+    
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  })
+
+  useEffect(() => {
+    if(scrollData.y > hideNavHeight){
+      setShowNav(true);
+    } else {
+      setShowNav(false);
+    }
+
+    if(scrollData.lastY < scrollData.y){
+      setShowNav(true);
+    } else {
+      setShowNav(false);
+    }
+  })
 
   return (
     <div>
-      <nav className='nav'>
+      <nav className={showNav ? 'nav hidden-nav' : 'nav'}>
         <HashLink to="#home" className='site-logo' onClick={() => scroll.scrollTo(document.querySelector('#home').offsetTop, { duration: scrollDuration })}>
           <img src={require('../assets/mushroom.png')} alt='logo' className='logo'/>
         </HashLink>
