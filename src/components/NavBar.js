483bddd93c8f1382f859b6ea6e4d53ee649ad18f
Sam Smith <scs5@clemson.edu>
2023-05-02 20:07:24 -0400

    Update navigation bar
    Styles navigation bar and adds smooth scrolling animations.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,32 @@
 import React from 'react'
-import { HashLink as Link } from 'react-router-hash-link';
+import { HashLink } from 'react-router-hash-link';
+import { animateScroll as scroll } from 'react-scroll';
 import "./NavBar.css"
 
 const NavBar = () => {
+  const scrollDuration = 500;
+
   return (
     <div>
       <nav className='nav'>
-        <Link to="#home" className='site-logo'>
+        <HashLink to="#home" className='site-logo' onClick={() => scroll.scrollTo(document.querySelector('#home').offsetTop, { duration: scrollDuration })}>
           <img src={require('../assets/mushroom.png')} alt='logo' className='logo'/>
-        </Link>
+        </HashLink>
         <ul>
           <li>
-            <Link to="#about">
-              About
-            </Link>
+            <HashLink to="#home" onClick={() => scroll.scrollTo(document.querySelector('#home').offsetTop, { duration: scrollDuration })}>
+              Home
+            </HashLink>
           </li>
           <li>
-            <Link to="#contact">
+            <HashLink to="#about" onClick={() => scroll.scrollTo(document.querySelector('#about').offsetTop, { duration: scrollDuration })}>
+              About
+            </HashLink>
+          </li>
+          <li>
+            <HashLink to="#contact" onClick={() => scroll.scrollTo(document.querySelector('#contact').offsetTop, { duration: scrollDuration })}>
               Contact
-            </Link>
+            </HashLink>
           </li>
         </ul>
       </nav>
