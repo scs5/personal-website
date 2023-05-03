f79226a1c313a4c449f856e65c7f3d50de42f446
scsmit23 <scs5@clemson.edu>
2023-05-03 16:33:52 -0400

    Update social icons

--- src/pages/Socials.js
+++ src/pages/Socials.js
@@ -5,10 +5,10 @@ function Socials() {
   return (
     <div className="social-menu">
       <div className="social-icon">
-        <a href="https://www.linkedin.com/in/sam-cooper-smith/"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://www.linkedin.com/in/sam-cooper-smith/"><i class="fa fa-linkedin"></i></a>
       </div>
       <div className="social-icon">
-        <a href="https://github.com/scs5"><i class="fa-brands fa-github"></i></a>
+        <a href="https://github.com/scs5"><i class="fa fa-github"></i></a>
       </div>
     </div>
   )
