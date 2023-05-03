36024ca6460bd7042013c55be7c741e1ad400042
Sam Smith <scs5@clemson.edu>
2023-05-03 01:25:39 -0400

    Make navigation bar mobile-compatible
    Adds dropdown navigation menu when screen is narrow enough (mobile).

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,44 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { HashLink } from 'react-router-hash-link';
 import { animateScroll as scroll } from 'react-scroll';
-import resume from '../assets/Sam_Smith_Resume.pdf'
-import "./NavBar.css"
+import resume from '../assets/Sam_Smith_Resume.pdf';
+import './NavBar.css';
 
 const NavBar = () => {
   const scrollDuration = 500; // how long autoscroll takes
-  const hideNavHeight = 300;  // height at which navbar disappears
-
-  const [ showNav, setShowNav ] = useState(true);
-  const [ scrollData, setScrollData ] = useState( {
+  const hideNavHeight = 300; // height at which navbar disappears
+  const [showNav, setShowNav] = useState(true);
+  const [scrollData, setScrollData] = useState({
     y: 0,
-    lastY: 0
-  })
+    lastY: 0,
+  });
+  const [dropdown, setDropdown] = useState(false);
 
   // handling disappearing navbar
-  useEffect(() =>{
+  useEffect(() => {
     const handleScroll = () => {
-      setScrollData(prevState => {
+      setScrollData((prevState) => {
         return {
           y: window.scrollY,
-          lastY: prevState.y
-        }
-      })
-    }
-    
-    window.addEventListener('scroll', handleScroll)
-    return () => window.removeEventListener('scroll', handleScroll)
+          lastY: prevState.y,
+        };
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   });
 
   useEffect(() => {
     // show navbar if once scrolled down far enough
-    if(scrollData.y > hideNavHeight){
+    if (scrollData.y > hideNavHeight) {
       setShowNav(true);
     } else {
       setShowNav(false);
     }
 
     // show navbar if user scrolls us
-    if(scrollData.lastY < scrollData.y){
+    if (scrollData.lastY < scrollData.y) {
       setShowNav(true);
     } else {
       setShowNav(false);
@@ -48,35 +48,71 @@ const NavBar = () => {
   return (
     <div>
       <nav className={showNav ? 'nav hidden-nav' : 'nav'}>
-        <HashLink to="#home" className='site-logo' onClick={() => scroll.scrollTo(document.querySelector('#home').offsetTop, { duration: scrollDuration })}>
-          <img src={require('../assets/mushroom.png')} alt='logo' className='logo'/>
+        <HashLink
+          to="#home"
+          className="site-logo"
+          onClick={() =>
+            scroll.scrollTo(document.querySelector('#home').offsetTop, {
+              duration: scrollDuration,
+            })
+          }
+        >
+          <img src={require('../assets/mushroom.png')} alt="logo" className="logo" />
         </HashLink>
-        <ul>
-          <li>
-            <HashLink to="#home" onClick={() => scroll.scrollTo(document.querySelector('#home').offsetTop, { duration: scrollDuration })}>
-              Home
-            </HashLink>
-          </li>
-          <li>
-            <HashLink to="#about" onClick={() => scroll.scrollTo(document.querySelector('#about').offsetTop, { duration: scrollDuration })}>
-              About
-            </HashLink>
-          </li>
-          <li>
-            <HashLink to="#contact" onClick={() => scroll.scrollTo(document.querySelector('#contact').offsetTop, { duration: scrollDuration })}>
-              Contact
-            </HashLink>
-          </li>
-          <li>
-            <a href={resume} target="_blank" rel='noopener noreferrer'>
-              <i className="fa fa-file-pdf-o"></i>
-              <span className='resume-link'>Resume</span>
-            </a>
-          </li>
-        </ul>
+
+        <a className="toggle-button" onClick={() => setDropdown(!dropdown)}>
+          <i className="fa fa-bars"></i>
+        </a>
+
+        <div className={`navbar-links ${dropdown ? 'active' : ''}`}>
+          <ul>
+            <li>
+              <HashLink
+                to="#home"
+                onClick={() =>
+                  scroll.scrollTo(document.querySelector('#home').offsetTop, {
+                    duration: scrollDuration,
+                  })
+                }
+              >
+                Home
+              </HashLink>
+            </li>
+            <li>
+              <HashLink
+                to="#about"
+                onClick={() =>
+                  scroll.scrollTo(document.querySelector('#about').offsetTop, {
+                    duration: scrollDuration,
+                  })
+                }
+              >
+                About
+              </HashLink>
+            </li>
+            <li>
+              <HashLink
+                to="#contact"
+                onClick={() =>
+                  scroll.scrollTo(document.querySelector('#contact').offsetTop, {
+                    duration: scrollDuration,
+                  })
+                }
+              >
+                Contact
+              </HashLink>
+            </li>
+            <li>
+              <a href={resume} target="_blank" rel="noopener noreferrer" className='resume-button'>
+                <i className="fa fa-file-pdf-o"></i>
+                <span className="resume-link">Resume</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
