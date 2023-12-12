722e2c2e6c3753d4f5d62335276612bd8f68fafa
Sam Smith <scs5@clemson.edu>
2023-12-12 18:54:47 -0500

    Added DBMS project

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -23,4 +23,16 @@ export const projectData = [
     categories: ["Misc"],
     description: "Have you ever wondered which skills are in demand in the job market? Are you looking to enhance your skill set for a specific career path? Look no further! This project is all about automating the process of extracting skills from job listings on Indeed, categorizing them, and diving deep into data analysis to uncover valuable insights. See my repository below for more details."
   },
+  {
+    id: 3,
+    name: "Wolfpack Parking Database",
+    imageSrc: require("../assets/wolfpack-database-logo.webp"),
+    popupSrc: require("../assets/wolfpack-database-logo.webp"),
+    links: [
+      { name: "GitHub", url: "https://github.com/scs5/ncsu-parking-db" }
+    ],
+    technologies: ["SQL", "Python"],
+    categories: ["Data Science"],
+    description: "In collaboration with a team of graduate students during a DBMS course, I created a database modeling NC State's parking system. Our project includes a straightforward user interface enabling users to perform various operations that, behind the scenes, run SQL queries via SQLite to manage the data effectively."
+  },
 ];
