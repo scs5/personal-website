dcebbed114419be7c53d1bfd2fcd03fdfbbe008b
scsmit23 <scs5@clemson.edu>
2024-01-07 11:45:16 -0500

    Added project and updated project banners

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,8 +2,8 @@ export const projectData = [
   {
     id: 1,
     name: "Portfolio Website",
-    imageSrc: require("../assets/website-project-img.webp"),
-    popupSrc: require("../assets/website-header.webp"),
+    imageSrc: require("../assets/website-logo.webp"),
+    popupSrc: require("../assets/website-banner.webp"),
     technologies: ["React", "JavaScript", "HTML", "CSS"],
     categories: ["Misc"],
     description: "This website right here! Instead of using a standard drag-and-drop website builder, I decided it would be more interesting and allow more customization if I were to code it up myself from scratch. I'm still very new to frontend development, but this has been a pretty fun experience and I've learned a lot.",
@@ -15,7 +15,7 @@ export const projectData = [
     id: 2,
     name: "Job Market Skills Analysis",
     imageSrc: require("../assets/indeed-scraper-logo.webp"),
-    popupSrc: require("../assets/indeed-scraper-logo.webp"),
+    popupSrc: require("../assets/indeed-scraper-banner.webp"),
     links: [
       { name: "GitHub", url: "https://github.com/scs5/indeed-scraper" }
     ],
@@ -27,7 +27,7 @@ export const projectData = [
     id: 3,
     name: "Wolfpack Parking Database",
     imageSrc: require("../assets/wolfpack-database-logo.webp"),
-    popupSrc: require("../assets/wolfpack-database-logo.webp"),
+    popupSrc: require("../assets/wolfpack-database-banner.webp"),
     links: [
       { name: "GitHub", url: "https://github.com/scs5/ncsu-parking-db" }
     ],
@@ -39,12 +39,24 @@ export const projectData = [
     id: 4,
     name: "Sign Language Detection",
     imageSrc: require("../assets/asl-detection-logo.webp"),
-    popupSrc: require("../assets/asl-detection-logo.webp"),
+    popupSrc: require("../assets/asl-detection-banner.webp"),
     links: [
       { name: "GitHub", url: "https://github.com/scs5/sign-language-detection" }
     ],
     technologies: ["Scikit-learn", "OpenCV", "Python"],
     categories: ["Data Science"],
     description: "This project aims to create a real-time sign language detection system using a custom dataset of hand signs. This involves capturing multiple images of different hand signs, extracting hand landmarks for feature representation, and training a classification model to predict the sign. The trained model is then deployed for real-time sign language detection."
+  },
+  {
+    id: 5,
+    name: "Cirrhosis Outcome Prediction",
+    imageSrc: require("../assets/cirrhosis-prediction-logo.webp"),
+    popupSrc: require("../assets/cirrhosis-prediction-banner.webp"),
+    links: [
+      { name: "GitHub", url: "https://github.com/scs5/cirrhosis-prediction" }
+    ],
+    technologies: ["Scikit-learn", "Optuna", "Python"],
+    categories: ["Data Science"],
+    description: "This was my first Kaggle competition where the task was to predict the outcomes of cirrhosis patients. I placed in the top 15% by creating a simple classification pipeline using XGBoost, Optuna hyperparameter tuning, and experimenting with autoencoders."
   },
 ];
