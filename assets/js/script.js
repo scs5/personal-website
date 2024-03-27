a1abd940ddfa3f269dd5a3f01ee97109fa7cabcb
Sam Smith <60248932+scs5@users.noreply.github.com>
2024-03-27 09:26:28 -0400

    Updated portfolio style

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,10 +81,12 @@ const filterItems = document.querySelectorAll("[data-filter-item]");
 const filterFunc = function (selectedValue) {
 
   for (let i = 0; i < filterItems.length; i++) {
+    console.log(selectValue.textContent.toLowerCase())
+
 
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
+    } else if (filterItems[i].dataset.category.includes(selectValue.textContent.toLowerCase())) {
       filterItems[i].classList.add("active");
     } else {
       filterItems[i].classList.remove("active");
